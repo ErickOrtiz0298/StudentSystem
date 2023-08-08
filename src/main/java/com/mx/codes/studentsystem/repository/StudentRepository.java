83cd5d4763d3adfbf9728c17c85df19cdf373e20
ErickOrtiz0298 <a13003218@alumnos.uady.mx>
2023-08-07 20:13:18 -0600

    Se añade la notación repository y se extiende de JPA llave Student valor integer

--- src/main/java/com/mx/codes/studentsystem/repository/StudentRepository.java
+++ src/main/java/com/mx/codes/studentsystem/repository/StudentRepository.java
@@ -1,4 +1,10 @@
 package com.mx.codes.studentsystem.repository;
 
-public interface StudentRepository {
+import com.mx.codes.studentsystem.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+
 }
