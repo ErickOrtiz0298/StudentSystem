1cda133ddd2ad93a751d8e3ca638e46fdf5d5d65
ErickOrtiz0298 <a13003218@alumnos.uady.mx>
2023-08-07 21:42:00 -0600

    metodo para guardar a un estudiante saveStudent

--- src/main/java/com/mx/codes/studentsystem/service/StudentService.java
+++ src/main/java/com/mx/codes/studentsystem/service/StudentService.java
@@ -1,4 +1,8 @@
 package com.mx.codes.studentsystem.service;
 
+import com.mx.codes.studentsystem.model.Student;
+
 public interface StudentService {
+    public Student saveStudent(Student student);
+
 }
