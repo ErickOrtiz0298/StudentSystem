ae4802e80925529406d8ef0a37597caaa9afe4c5
ErickOrtiz0298 <a13003218@alumnos.uady.mx>
2023-08-07 21:42:49 -0600

    Implementacion del metodo saveStudent

--- src/main/java/com/mx/codes/studentsystem/service/StudentServiceImpl.java
+++ src/main/java/com/mx/codes/studentsystem/service/StudentServiceImpl.java
@@ -1,4 +1,14 @@
 package com.mx.codes.studentsystem.service;
 
-public class StudentServiceImpl {
+import com.mx.codes.studentsystem.model.Student;
+import com.mx.codes.studentsystem.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class StudentServiceImpl implements StudentService{
+    @Autowired
+    private StudentRepository studentRepository;
+    @Override
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
 }
